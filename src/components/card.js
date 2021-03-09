50a04d69ca9374d7430f5394cbd563ee5df3a1c9
eilla1 <exu6056@gmail.com>
2021-03-08 23:44:49 -0800

    :building_construction: Add hover on cards

--- src/components/card.js
+++ src/components/card.js
@@ -8,7 +8,7 @@ const Card = () => (
      <Box m={20} bg="white" borderRadius={20} color="black" p={10} border="2px" borderColor="gray.100">
         <Text fontWeight="bold" fontSize="4xl" textAlign="center">Get Started</Text><br/>
         <Flex justifyContent="center" mx="auto" flexDirection="row" flexWrap="wrap">
-          <Box borderRadius={20} bg="pink.100" p={5} mr={5} mb={5}>
+          <Box borderRadius={20} bg="pink.100" p={5} mr={5} mb={5} _hover={{ transform: "scale(1.1)" }}>
               <VStack alignItems="start">
                   <Image src="https://cloud-9qzt9qmx7-hack-club-bot.vercel.app/0vector-1.png" /><br/>
                   <Link to="/guidelines"> 
@@ -16,7 +16,7 @@ const Card = () => (
                   </Link>
               </VStack>
           </Box>
-          <Box borderRadius={20} bg="blue.100" p={5} mr={5} mb={5}>
+          <Box borderRadius={20} bg="blue.100" p={5} mr={5} mb={5} _hover={{ transform: "scale(1.1)" }}>
               <VStack alignItems="start">
                   <Image src="https://cloud-9qzt9qmx7-hack-club-bot.vercel.app/1vector-2.png" /><br/>
                   <Link to="/submit">
@@ -24,12 +24,12 @@ const Card = () => (
                   </Link>
               </VStack>
           </Box> 
-          <Box borderRadius={20} bg="purple.100" p={5} mb={5}>
+          <Box borderRadius={20} bg="purple.100" p={5} mb={5} _hover={{ transform: "scale(1.1)" }}>
               <VStack alignItems="start">
                   <Image src="https://cloud-9qzt9qmx7-hack-club-bot.vercel.app/2vector.png" /><br/>
-                  <Link to="/gallery">
+                  <a href="https://airtable.com/shrMBfy9t74j8PAxc">
                       <Text fontSize="2xl">View the gallery →</Text>
-                  </Link>
+                  </a>
               </VStack>
           </Box>  
         </Flex>
