79c7cfbb3ed2e4fb11618c8664e275411a801eb0
eilla1 <exu6056@gmail.com>
2021-03-11 22:25:30 -0800

    :lipstick: Update styles

--- src/pages/submit.js
+++ src/pages/submit.js
@@ -2,10 +2,12 @@ import { ChakraProvider, Box } from "@chakra-ui/react"
 import Header from '../components/header'
 import Footer from '../components/footer'
 import React from "react"
+import Head from '../components/head'
 
 export default function Submit() {
     return (
         <ChakraProvider>
+            <Head />
             <Header />
             <Box>
             <iframe title="Submit a project" src="https://airtable.com/embed/shrxwr2YZHePwD5wz?backgroundColor=cyan" width="100%" height="1000px"></iframe>
