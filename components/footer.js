54bfad89fd8bd3dc53920d55736e44623283fd57
Ella <72365100+eilla1@users.noreply.github.com>
2021-04-22 21:46:02 -0700

    feat: add footer icons (#33)

--- components/footer.js
+++ components/footer.js
@@ -1,3 +1,6 @@
+import { FiGithub } from "react-icons/fi"
+import { RiCommandFill } from "react-icons/ri"
+import { CgGirl } from "react-icons/cg"
 export default function Footer() {
   return (
     <div className="text-gray-500 text-center pb-8">
@@ -5,6 +8,17 @@ export default function Footer() {
       <p className="uppercase font-semibold text-gray-300">
         Copyleft Command Tech x Girl Genius 2021.
       </p>
+      <span className="flex justify-center p-5 space-x-3">
+        <a href="https://github.com/eilla1/InnovatHER-website">
+          <FiGithub size={30} />
+        </a>
+        <a href="https://commandtech.codes">
+          <RiCommandFill size={30} />
+        </a>
+        <a href="https://girlgeniusmag.tech">
+          <CgGirl size={30} />
+        </a>
+      </span>
     </div>
   )
 }
